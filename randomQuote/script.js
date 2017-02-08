b758f9ce1ec3087779a54d8842a05ff589847b08
roerjo <roerjo.personal@gmail.com>
2017-02-07 22:51:43 -0500

    connected to api

---
+++ randomQuote/script.js
@@ -0,0 +1,28 @@
+var quote = document.getElementById('quote');
+var author = document.getElementById('author');
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET','http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en');
+xhr.responseType = 'json';
+xhr.send(null);
+
+xhr.onreadystatechange = function(e) {
+    var DONE = 4;
+    var OK = 200;
+    if(xhr.readyState === DONE) {
+        if(xhr.status === OK){
+            quote.innerHTML = e.target.response.quoteText;
+            author.innerHTML = e.target.response.quoteAuthor;
+        }else 
+        console.log('Error: ' + xhr.status);
+    }
+}; 
+
+window.onload = function() {
+    var newQuote = document.getElementById('newQuote');
+    newQuote.addEventListener("click", function() {
+    xhr.open('GET','http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en');
+    xhr.responseType = 'json';
+    xhr.send();
+    });
+}
